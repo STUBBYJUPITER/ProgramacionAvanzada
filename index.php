f19fa228500cde201c27d21e148a20db269c8ea1
stubby <92502112+STUBBYJUPITER@users.noreply.github.com>
2022-08-17 17:50:51 -0600

    add activity
    add mouseEvents and random rgb in the php file

--- index.php
+++ index.php
@@ -7,70 +7,64 @@
     <title>Canvas</title>
     <style type="text/css">
         canvas{
-            background-color: red;
+            background-color: silver;
         }
     </style>
 </head>
 <body>
     <h1>practica de programacion avanzada</h1>
     <canvas id="canvas" width="500" height="500">
+        <img src="wolf.png" id="imagen" width="40" height="40">
         <script type="text/javascript">
 
             // para que el navegador soporte canvas
             var canvas = document.getElementById("canvas");
             var ctx = canvas.getContext('2d');
+            var color="rgb(0,0,0)";
+            var fig="arc";
 
-            ctx.fillStyle = 'black';
-            ctx.fillRect(10, 10, 100, 100);
-            //cuadro completo usando transparencia
-            ctx.fillStyle = 'rgba(0, 0, 255, 0.5)';
-            ctx.fillRect(90, 90, 100, 100);
-            ctx.fillStyle = 'rgba(20, 20, 200, 0.5)';
-            ctx.fillRect(180, 180, 100, 100);
-
-            //contorno de un cuadrado
-            // ctx.strokeStyle="black";
-            //ctx.strokeRect(10,10,200,200);
-
-            //ctx.strokeStyle="green";
-            //ctx.strokeRect(30,30,200,200);
-
-           //stroke = contorno de algo
-           //lineas
-
-           //donde empieza
-           ctx.moveTo(0,0);
-           //donde termina
-           ctx.lineTo(500,500);
-           //traza la linea
-           ctx.stroke();
-
-/*
-           ctx.moveTo(0,0);
-           ctx.lineTo(400,20);
-           ctx.lineTo(150,150);
-           ctx.stroke();
-           ctx.fillStyle = 'rgba(150, 150, 100, 0.5)';
-           ctx.fill();
            
 
+            var img=document.getElementById("imagen");
+            ctx.drawImage(img,20,50);
+
+    
+            canvas.addEventListener('click',function(e){
+                console.log(e);
+                ctx.fillStyle = color;
+                if(fig=="arc"){
+                    ctx.beginPath();
+                    ctx.arc(e.offsetX-5,e.offsetY-5,50,0,2*Math.PI);
+                    ctx.strokeStyle="black";
+                    
+                }else{
+                    ctx.beginPath();
+                    ctx.fillRect(e.offsetX-20,e.offsetY-20,50,50);
+                    
+                }
+                ctx.fill();
+                ctx.stroke();
+
+            });
+    
+    canvas.addEventListener('mouseover',function(e){
+           
+            color=random_rgba();
           
-           ctx.beginPath();
-           ctx.fillStyle = 'green';
-           ctx.arc(100,390,100,0,2*Math.PI);
-           ctx.fill();
+        });
 
-           ctx.beginPath();
-           ctx.fillStyle = 'purple';
-           ctx.arc(390,390,100,0,2*Math.PI);
-           ctx.fill();
-*/
-           //texto
-           ctx.beginPath();
-           ctx.font="30px Arial";
-           ctx.fillText("hola que hace",200,100);
+    canvas.addEventListener('mouseout',function(e){
+           
+            fig=(fig=="arc")?"rec":"arc";
+          
+        });
+   
 
-           ctx.strokeText("nada",340,140);
+    function random_rgba() {
+    var o = Math.round, r = Math.random, s = 255;
+    return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+    }
+
 
 
         </script>
