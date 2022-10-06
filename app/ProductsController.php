fa3eae83ebdaad3cc575ab490036cebf51b33897
stubby <pandorum117@gmail.com>
2022-10-05 18:02:02 -0600

    ?

--- app/ProductsController.php
+++ app/ProductsController.php
@@ -1,5 +1,37 @@
 <?php
 session_start();
+if (isset($_POST["addProduct"])) {
+    switch ($_POST["addProduct"]) {
+        case 'done':
+            $name = strip_tags($_POST['name']);
+            $description = strip_tags($_POST['description']);
+            $features = strip_tags($_POST['features']);
+            $brand_id = strip_tags($_POST['brand_id']);
+
+            if ($_FILES["addImage"]["error"] > 0) {
+                echo "error to update image";
+            } else {
+                $allowed = array("image/gif", "image/jpg", "image/png");
+                if (in_array($_FILES["addImage"]["type"], $allowed)) {
+                    $route = "file/";
+                    $file = $route . $_FILES["addImage"]["name"];
+                    if (!file_exists($route)) {
+                        mkdir($route);
+                    }
+                }
+            }
+            $newProduct = new ProductsController();
+            $newProduct->addProduct($name, $description, $features, $brand_id, $file);
+            break;
+        case 'delete':
+            $id=strip_tags($_POST['id']);
+            $newProduct = new ProductsController();
+            echo json_encode($newProduct->deleteProduct($id));
+
+
+            break;
+    }
+}
 class ProductsController
 {
     public function getProducts()
@@ -68,9 +100,9 @@ class ProductsController
 
         if (isset($response->code) && $response->code > 0) {
 
-            header("location:../products/index.php");
+            header("location:../products?success");
         } else {
-            echo "try again";
+            header("location:../products?error");
         }
     }
     public function getProductBySlug($product_slug)
@@ -103,29 +135,35 @@ class ProductsController
             header("location:../products/error.php");
         }
     }
-}
-if (isset($_POST["addProduct"])) {
-    switch ($_POST["addProduct"]) {
-        case 'done':
-            $name = strip_tags($_POST['name']);
-            $description = strip_tags($_POST['description']);
-            $features = strip_tags($_POST['features']);
-            $brand_id = strip_tags($_POST['brand_id']);
+    public function deleteProduct($id){
+        $curl = curl_init();
 
-            if ($_FILES["addImage"]["error"] > 0) {
-                echo "error to update image";
-            } else {
-                $allowed = array("image/gif", "image/jpg", "image/png");
-                if (in_array($_FILES["addImage"]["type"], $allowed)) {
-                    $route = "file/";
-                    $file = $route . $_FILES["addImage"]["name"];
-                    if (!file_exists($route)) {
-                        mkdir($route);
-                    }
-                }
-            }
-            $newProduct = new ProductsController($name, $description, $features, $brand_id, $file);
-            $newProduct->addProduct($name, $description, $features, $brand_id, $file);
-            break;
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://crud.jonathansoto.mx/api/product/'.$id,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'DELETE',
+        CURLOPT_HTTPHEADER => array(
+            'Authorization: Bearer ' . $_SESSION['token'],
+            
+        ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        $response = json_decode($response);
+
+        if (isset($response->code) && $response->code > 0) {
+
+            return true;
+        } else {
+            return false;
+        }
+
     }
 }
--- products/index.php
+++ products/index.php
@@ -1,132 +1,140 @@
-<?php
-$slug = $_GET['slug'];
-
+<?php 
 include '../app/ProductsController.php';
-$productsController = new ProductsController();
-//$products=$productsController->getProducts();
-$product = $productsController->getProductBySlug($slug);
+include '../app/BrandsController.php';
+$brandController=new BrandController();
+$brands=$brandController->getBrands();
+$productsContoller=new ProductsController();
+$products=$productsContoller->getProducts();
 ?>
-<!DOCTYPE html>
 <html>
-
+<!DOCTYPE html>
 <head>
-	<?php
-	include('../layout/head.template.php');
-	?>
+	<?php include '../layouts/head.template.php'; ?>
 </head>
-
+<style>
+	.btn-warning {
+		background-color: #8F3A84 !important;
+	}
+	.btn-primary {
+		background-color: #CD05A6 !important;
+	}
+</style>
 <body>
-
-	<!-- NAVBAR -->
-	<?php
-	include('../layout/navbar.template.php');
-	?>
-	<!-- NAVBAR -->
-
+	<?php include '../layouts/nav.template.php'; ?>
 	<div class="container-fluid">
-
 		<div class="row">
-
-			<!-- SIDEBAR -->
-			<?php
-			include('../layout/sidebar.template.php');
-			?>
-			<!-- SIDEBAR -->
-
+			<?php include '../layouts/sidebar.template.php'; ?>
+			
 			<div class="col-md-10 col-lg-10 col-sm-12">
-
-				<div class="col-md-10 col-lg-10 col-sm-12">
-
-					<section>
-						<div class="row bg-light m-2">
-							<div class="col">
-								<label>
-									/<a href="index.php">Products</a>/<?= $product->slug ?>
-								</label>
-							</div>
-
+				<section>
+					<div class="row bg-light m-2">
+						<div class="col">
+							<label>
+								products
+							</label>
 						</div>
-					</section>
-					<div class="container mt-5 mb-5">
-						<div class="row d-flex justify-content-center">
-							<div class="col-md-10">
-								<div class="card">
-									<div class="row">
-										<div class="col-md-6">
-											<div class="images p-3">
-												<div class="text-center p-4"> <img id="main-image" src="<?= $product->cover ?>" width="250" /> </div>
-											</div>
-										</div>
-										<div class="col-md-6">
-											<div class="product p-4">
-												<div class="d-flex justify-content-between align-items-center">
-												</div>
-												<div class="mt-4 mb-3"> <span class="text-uppercase text-muted brand"><?= $product->brand->name ?> </span>
-													<h5 class="text-uppercase"><?= $product->name ?></h5>
+						<div class="col">
+							<button data-bs-toggle="modal" data-bs-target="#addProductModal" class=" float-end btn btn-primary">
+								add products
+							</button>
+						</div>
+					</div>
+				</section>
+				<section>
+					<div class="row">
+						<?php if  (isset($products)&& count($products)) : ?>
+							<?php foreach($products as $product): ?>
+							<div class="col-md-4 col-sm-12">
+								<div class="card mb-2">
+									<img src="<?= $product -> cover ?>" class="card-img-top" alt="...">
+									<div class="card-body">
+										<h5 class="card-title"><?= $product -> name ?></h5>
+										<h6 class="card-subtitle mb-2 text-muted"><?= $product -> description ?></h6>
+										<p class="card-text"><?= $product -> features ?></p>
 
-												</div>
-												<p class="about"><?= $product->description ?></p>
-												<h6 class="text-uppercase">Caracteristicas</h5>
-													<p class="about"><?= $product->features ?></p>
-
-													<h5 class="text">Categorias</h5>
-													<?php if (isset($product->categories) && count($product->categories)) : ?>
-														<?php foreach ($product->categories as $category) : ?>
-
-															<a class="btn btn-primary mb-3" href="categorias.php?category=<?= $category->name ?>" role="button"><?= $category->name ?></a>
-														<?php endforeach; ?>
-													<?php endif; ?>
-
-													<h5 class="text">Etiquetas</h5>
-													<?php if (isset($product->tags) && count($product->tags)) : ?>
-														<?php foreach ($product->tags as $tag) : ?>
-
-															<a class="btn btn-primary mb-3" href="etiquetas.php?tag=<?= $tag->name ?>" role="button"><?= $tag->name ?></a>
-														<?php endforeach; ?>
-													<?php endif; ?>
-
-													<div class="row">
-														<a data-bs-toggle="modal" data-bs-target="#addProductModal" href="#" class="btn btn-warning mb-1 me-1 col-5">
-															Editar
-														</a>
-														<a onclick="eliminar(this)" href="#" class="btn btn-danger mb-1 ms-1 col-5">
-															Eliminar
-														</a>
-													</div>
-											</div>
+										<div class="row">
+											<a data-bs-toggle="modal" data-bs-target="#addProductModal" href="#" class="btn btn-warning mb-1 col-6">
+												edit
+											</a>
+											<a onclick="eliminar(<?= $product->id?>)" href="#" class="btn btn-danger mb-1 col-6">
+												delete
+											</a>
+											<a href="details.php" class="btn btn-info col-12">
+												details
+											</a>
 										</div>
 									</div>
 								</div>
 							</div>
-						</div>
+						<?php endforeach; ?>
+						<?php endif; ?>
 					</div>
-				</div>
+				</section>
 			</div>
 		</div>
+	</div>
+	<!-- Modal -->
+	<!-- action addProduct  -->
+	<div class="modal fade" id="addProductModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="exampleModalLabel">Add Product</h5>
+					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+				</div>
+				<form action="addProduct" method="post">
+					<div class="modal-body align-text-bottom">
+						
+					<input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" placeholder="name" required>
+					<input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" placeholder="slug" required>
+					<input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" placeholder="description" required>
+					<input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" placeholder="features" required>
+					<select name="brand_id" required class="form-control">
+				
 
-		<?php
-		include('../layout/scripts.template.php');
-		?>
-		<script type="text/javascript">
-			function eliminar(target) {
-				swal({
-						title: "Are you sure?",
-						text: "Once deleted, you will not be able to recover this imaginary file!",
-						icon: "warning",
-						buttons: true,
-						dangerMode: true,
-					})
-					.then((willDelete) => {
-						if (willDelete) {
-							swal("Poof! Your imaginary file has been deleted!", {
-								icon: "success",
-							});
-						} else {
-							swal("Your imaginary file is safe!");
-						}
-					});
-			}
+
+					</select>
+					<input type="number" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" placeholder="brand" required>
+					<input type="" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" placeholder="brand" required>
+
+
+
+						
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+							Close
+						</button>
+						<button type="submit" class="btn btn-primary">
+							Save changes
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	<?php include '../layouts/scripts.template.php' ?>
+	<script type="text/javascript">
+		function eliminar(id) {
+			swal({
+					title: "Are you sure?",
+					text: "Once deleted, you will not be able to recover this imaginary file!",
+					icon: "warning",
+					buttons: true,
+					dangerMode: true,
+				})
+				.then((willDelete) => {
+					if (willDelete) {
+						
+						swal("Poof! Your imaginary file has been deleted!", {
+							icon: "success",
+						});
+					} else {
+						swal("Your imaginary file is safe!");
+					}
+				});
+		}
+		
 		</script>
-</body>
-
+	</body>
 </html>
